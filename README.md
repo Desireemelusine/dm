7bb62d968e50a661cfa6e40c170d77cd0d70de2e
Desireemelusine <demelusine@gmail.com>
2018-10-22 18:41:59 +0100

    update file

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# dm
+# web
+## My Portfolio - Introduction
+
+After I got my graduation on my Nanodegree as a Front-End Developer I decided to challenge myself with a new project. Build my portfolio based on a Bootstrap Template that already exists and change completely to my needs.
+
+In a way gave me the feeling of refactoring the code, cleaning what wasn't need, add javaScript, work on CSS and create new items. I had to do a lot of research on things that I didn't know and learn before making any changing. I loved the challenge and it gave me the confidence to know that I'm capable to read tons of code and understand the importance of each line of code and make the decision to change or not.
+
+### What I've Done
+
+I bought the template from  https://wrapbootstrap.com/theme/vitality-one-page-bootstrap-4-theme-WB02K3KK3
+
+The first step was:
+* I knew Bootstrap 3 so I took time to understand Bootstrap 4 before start
+* Took some time to understand vitality.js and JS/ files
+* I planned the project on paper, including images, links, buttons etc.
+
+The second step was | Mobile First :
+* The project was built first for mobile
+* Cleaning code for HTML - I decided to put all the information on Js/Main.js for an easy update on the information without the need to open the HTML.
+* New file  Js/Main.js - for data (KnockoutJS) and javaScript function - toggle
+* I made some changes on the Js/vitality.js for the benefit of the Portfolio** and based on the needs
+* Reorganization and clean code for CSS/style.css, also some new lines
+* @ mediaquery - I had careful attention on this matter to be sure it would follow the resolution and devices.
+
+** Portfolio
+The original presentation did not allow to add the link, either open it because it had an animation that blocked the access to the link.
+
+ Thanks!
+
+
+####Attention Project Architecture - "Bootstrap | JQuery | javaScript" : Due to the fact we are working with API. Some parts of the page may not upload correctly. "This request has been blocked; the content must be served over HTTPS." If that is the case is recommended to download the project.( click on the link GitHub)
---
+++ js/main.js
@@ -0,0 +1,135 @@
+
+  /*---------------------VIEWMODEL------------------------------------------ */
+ function ViewModel() {
+     this.mTitle = "Désirée Melusine";
+     this.mSubtitle = "Front-End Developer & Designer";
+     this.sProfile = "Hi, I'm Désirée Melusine";
+     this.sResume = "I am a front-end developer and a product designer based in Lisbon, with a degree in Industrial Design and a NanoDegree in Front-End Web Developer. I have a professional background in product design and development, focused on product lifecycle and user experience for small companies, startups, and entrepreneurs.";
+     this.ssTitle = "What I do";
+     this.ssDescription = "Before I start working I like to understand the whole concept that is behind so I can get the main purpose of the project and thus, give my best to achieve and exceed expectations.";
+     this.ssSubtitle = "What I'm learning now : React";
+     this.ssFrontTitle = "Front-End Developer";
+     this.ssFrontDescrip = "A responsive website or app for desktop and mobile, thinking always on optimization and CRP (Critical Rendering Path). With a clean code, allowing third-party API integration and build based on MVC framework.";
+     this.ssWebTitle = "Web Design";
+     this.ssWebDescrip = "Webpages with CMS (Content Management System), based on templates for website and ecommerce for specific plataforms.";
+     this.ssDesTitle = "Product Design";
+     this.ssDesDescrip = "My experience is not just based on the product design itself, but on the whole concept, from the sketch, research, prototyping, testing until production, packaging, graphic and merchandising. Always focused on the user experience and interaction UX and UI. ";
+     this.ssWhTitle = "What Else";
+     this.ssWhDescrip = "I speak Portuguese (mother language) | English (Fluent) | French (Advanced) | Spanish (Intermediate). I have a Advanced Certification in Executive Coaching - High Perfomance by ICF because I believe that our perfomance is connect with our personnal life( our past and fears, balance with work and our self confidence), also our capacity to deal with pressure, control our inner, our way to communicate and also listen. ";
+     //grid1 FRONTEND - rest
+     this.g1FendImgSrc= "img/pt/grid/rest_300.jpg";
+     this.g1FendTitle = "A responsive and simple website build with Bootstrap for desktop and mobile";
+     this.g1FendDescrip = "HTML | CSS | Bootstrap 4.0";
+     this.g1FendGit= 'https://github.com/Desireemelusine/HTML-Bootstrap---Responsive';
+     this.g1FendUrl= 'https://desireemelusine.github.io/HTML-Bootstrap---Responsive/';
+     //grid2 FRONTEND - game
+     this.g2FendImgSrc= "img/pt/grid/lb_300.jpg";
+     this.g2FendTitle = "A game based on JavaScript object-oriented and the correct use of THIS";
+     this.g2FendDescrip = "Game | JavaScript OOP | Canvas";
+     this.g2FendGit= 'https://github.com/Desireemelusine/Sweet-Crazy-Ladybugs-Game';
+     this.g2FendUrl= 'https://desireemelusine.github.io/Sweet-Crazy-Ladybugs-Game/';
+     //grid3 FRONTEND - arch
+     this.g3FendImgSrc= "img/pt/grid/arch_300.jpg";
+     this.g3FendTitle = "A single web page based on data information. DOM is manipulated with jQuery";
+     this.g3FendDescrip = "Bootstrap | jQuery | JavaScript";
+     this.g3FendGit= 'https://github.com/Desireemelusine/Architecture_Portfolio_jQuery';
+     this.g3FendUrl= 'https://desireemelusine.github.io/Architecture_Portfolio_jQuery/';
+     //grid4 FRONTEND - feed
+     this.g4FendImgSrc= "img/pt/grid/fed_300.jpg";
+     this.g4FendTitle = "An application made by another developer. Jasmine was used for test-driven";
+     this.g4FendDescrip = "JavaScript | Jasmine Test-Driven";
+     this.g4FendGit= 'https://github.com/Desireemelusine/Jasmine-Test-Driven';
+     this.g4FendUrl= 'https://desireemelusine.github.io/Jasmine-Test-Driven';
+     //grid5 FRONTEND - cat
+     this.g5FendImgSrc= "img/pt/grid/cf_300.jpg";
+     this.g5FendTitle = "An app entirely focused on JavaScript MVC, with a premium option for the user to add his own url cat image, name and text.";
+     this.g5FendDescrip = "JavaScript MVC (Model-View-Controller) | jQuery | JSON";
+     this.g5FendGit= 'https://github.com/Desireemelusine/Cat-Clicker-Feeling';
+     this.g5FendUrl= 'https://desireemelusine.github.io/Cat-Clicker-Feeling';
+     //grid6 FRONTEND - map
+     this.g6FendImgSrc= "img/pt/grid/map_300.jpg";
+     this.g6FendTitle = "A single page application featuring a map of the neighborhood including highlighted locations, inviting the user to interact.";
+     this.g6FendDescrip = "GoogleAPI | KnockoutJS | Third-party API | Error Handling";
+     this.g6FendGit= 'https://github.com/Desireemelusine/My-Neighborhood';
+     this.g6FendUrl= 'https://desireemelusine.github.io/My-Neighborhood';
+     //grid1 DESIGN-fascolwin
+     this.g1DImgSrc= "img/pt/grid/facolwi_300.jpg";
+     this.g1DTitle = "Contemporary jewelry collection developed for the European Market";
+     this.g1DDescrip = "Porcelaine | Swarovski | Gold Plated and Gold leaf";
+     //grid2 DESIGN-facolsu
+     this.g2DImgSrc= "img/pt/grid/facolsu_300.jpg";
+     this.g2DTitle = "Contemporary jewelry collection developed for the International Market";
+     this.g2DDescrip = "Porcelaine | Swarovski | Gold Plated | Precious Stones";
+     //grid3 DESIGN-piacere
+     this.g3DImgSrc= "img/pt/grid/pi_300.jpg";
+     this.g3DTitle = "Logo, packaging, merchandising, catalog, website, ecommerce and logistics for a line of 27 gourmet products - tea, coffee, sweets and dragees.";
+     this.g3DDescrip = "Packaging | Catalog | Product & Logistic Development";
+     //grid3 DESIGN-merely
+     this.g4DImgSrc= "img/pt/grid/me_300.jpg";
+     this.g4DTitle = "Logo and packaging development for a new line of chocolates for a specific market. Special attention on the label based on the laws required by the country.";
+     this.g4DDescrip = "Packaging | Logo | Graphic | Product | Market Research";
+     //grid5 DESIGN-UX UI
+     this.g5DImgSrc= "img/pt/grid/home_app_300.jpg";
+     this.g5DTitle = "Sketch, Ux, research, prototyping, interface and interaction";
+     this.g5DDescrip = "UX | UI | Webpage | Ecommerce | App";
+     this.g5DUrl= 'https://www.figma.com/proto/0Jk2utb8kGODtZzQ0DcWuOc5/Home-app-Apple-Watch?node-id=1%3A2&scaling=scale-down';
+     //grid6 DESIGN-Logo
+     this.g6DImgSrc= "img/pt/grid/logo_300.jpg";
+     this.g6DTitle = "Some examples of logos for different companies";
+     this.g6DDescrip = "illustrator | Photoshop";
+
+ }
+
+/*---------------------VIEW------------------------------------------ */
+//portfolio -more info toggle
+ $(document).ready(function(){
+     $("#tg1").click(function(){
+         $("#tgO1").toggle();
+     });
+     $("#tg2").click(function(){
+         $("#tgO2").toggle();
+     });
+     $("#tg3").click(function(){
+         $("#tgO3").toggle();
+     });
+     $("#tg4").click(function(){
+         $("#tgO4").toggle();
+     });
+     $("#tg5").click(function(){
+         $("#tgO5").toggle();
+     });
+     $("#tg6").click(function(){
+         $("#tgO6").toggle();
+     });
+     $("#tg7").click(function(){
+         $("#tgO7").toggle();
+     });
+     $("#tg8").click(function(){
+         $("#tgO8").toggle();
+     });
+     $("#tg9").click(function(){
+         $("#tgO9").toggle();
+     });
+     $("#tg10").click(function(){
+         $("#tgO10").toggle();
+     });
+     $("#tg11").click(function(){
+         $("#tgO11").toggle();
+     });
+     $("#tg12").click(function(){
+         $("#tgO12").toggle();
+     });
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+ko.applyBindings(new ViewModel);
